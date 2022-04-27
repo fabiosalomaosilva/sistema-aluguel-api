690b094aba0b3572d1e79e305c749b445cd74dce
fabio@arquivarnet.com.br <fabio@arquivarnet.com.br>
2022-04-26 19:15:13 -0500

    update

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
-export const app = express();
+const app = express();
 
 app.use(express.json());
 
-app.get("/", (req:Request, res:Response) => {
-    res.status(201).json({name: "Fábio Salomão", age: "41", email: "fabio@arquivarnet.com.br"});
+app.get('/', (req: Request, res: Response) => {
+  res.status(201).json({
+    name: 'Fábio Salomão',
+    age: '41',
+    email: 'fabio@arquivarnet.com.br',
+  });
 });
+
+app.listen(3333);
--- server.ts
+++
@@ -1,5 +0,0 @@
-import { app } from "./app";
-
-app.listen(3333, () => {
-    console.log("API rodando na porta 3333");
-});
